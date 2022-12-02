e46c662cb3de431fd07859cbf6a2a0f82ae1a46a
ira-lapchuk <114941166+ira-lapchuk@users.noreply.github.com>
2022-12-02 13:41:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const myArr=[
+  5, 9, 37, 22, 49, 3
+]
+
+
+function isInArray(myArr) {
+    //for (let i = 0; i = myArr.lenght; i++) { // checking each item
+    for (myArr.includes(el)) {
+      result = true;
+      console.log(result);
+    }   
+    return result;
+  }
---
+++ script2.js
@@ -0,0 +1,6 @@
+const arrayNumber = [
+    2, 4, 33, 59, 11, 9, 41
+];
+
+console.table(arrayNumber);
+
